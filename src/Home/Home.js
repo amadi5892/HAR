4386e57afd5bce8b5fc81bdf19a51bf90b3558b5
Parrish Williamson <williamsonparrish@gmail.com>
2021-02-14 15:41:29 -0500

    added background image

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <div className="landing">
-
+                    
                 </div>
             </div>
         )
