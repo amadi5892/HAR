dd312b8980d02fffb80bf7e0b2b3cd5ba6d2ff7b
Parrish Williamson <williamsonparrish@gmail.com>
2021-02-19 18:41:03 -0500

    paragraphs setup for commitment

--- src/Commitment/Commitment.js
+++ src/Commitment/Commitment.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './Commitment.css';
 import Title from './Title/Title';
+import Paragraph from './Paragraph/Paragraph';
 
 const commitment = (props) => (
     <div className="commit">
         <Title />
+        <Paragraph />
     </div>
 );
 
---
+++ src/Commitment/Paragraph/Paragraph.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Paragraph.css';
+
+const paragraph = (props) => (
+    <div className="para-container">
+        <p className="para-1">
+            With over 30 years of experience, serving Queens, Brooklyn, Manhattan, Long Island and the Bronx, Home Affordable Realty has been right here in the neighborhood <strong>Selling</strong> and <strong>Buying</strong> homes for our clients.
+        </p>
+        <p className="para-2">
+
+        </p>
+    </div>
+);
+
+export default paragraph;
