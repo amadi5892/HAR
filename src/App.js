cdb7d2dc34f6d48c9336ef0e0410af940fec5e68
Parrish Williamson <williamsonparrish@gmail.com>
2021-02-19 21:46:21 -0500

    added Services component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Home from './Home/Home';
 import Commitment from './Commitment/Commitment';
+import Services from './Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Commitment />
+      <Services />
     </div>
   );
 }
---
+++ src/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Services.css';
+
+const services = (props) => (
+    <div className="service-container">
+
+    </div>
+);
+
+export default services;
