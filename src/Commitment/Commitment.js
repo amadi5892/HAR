86f66313bfb4311e3bc04cef25b704dbd65544d8
Parrish Williamson <williamsonparrish@gmail.com>
2021-02-19 19:34:41 -0500

    added image to commitment

--- src/Commitment/Commitment.js
+++ src/Commitment/Commitment.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './Commitment.css';
 import Title from './Title/Title';
 import Paragraph from './Paragraph/Paragraph';
+import CommitmentImage from './CommitmentImage/CommitmentImage';
 
 const commitment = (props) => (
     <div className="commit">
         <Title />
         <Paragraph />
+        <CommitmentImage />
     </div>
 );
 
---
+++ src/Commitment/CommitmentImage/CommitmentImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './CommitmentImage.css';
+import modernSunset from '../../images/modern.jpg';
+
+const commitmentImage = (props) => (
+    <img src={modernSunset} alt="Modern Home in the Sunset" className="img" ></img>
+);
+
+export default commitmentImage;
