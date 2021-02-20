108910fc0919bd530492e1f4e50814983cb18154
Parrish Williamson <williamsonparrish@gmail.com>
2021-02-20 11:31:20 -0500

    Services component added

---
+++ src/Services/Heading/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Heading.css';
+
+const heading = (props) => (
+    <h3 className="heading">Buyers & Sellers</h3>
+);
+
+export default heading;
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import './Services.css';
+import Heading from './Heading/Heading';
 
 const services = (props) => (
     <div className="service-container">
-
+        <Heading />
     </div>
 );
 
